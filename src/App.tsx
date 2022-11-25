30c4d8f08396bac31fbecdddeeb86049083e2bdf
charmant27 <charmantogatera@gmail.com>
2022-11-25 12:43:24 -0800

    All setup before the actual functionality were added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 import { useQuery } from "react-query";
+import { FaShoppingCart } from "react-icons/fa";
 
 //components
 import Item from "./Items/Item";
 import Navbar from "./components/Navbar";
-import Grid2 from '@mui/material/Unstable_Grid2'
-import Grid from '@mui/material/Grid';
+import Grid from "@mui/material/Grid";
+import Badge from "@mui/material/Badge";
+import { Drawer } from "@mui/material";
 
 //styled components
 import { Wrapper } from "./App.styles";
-
 
 //items types
 export type cartItemsType = {
@@ -27,17 +28,16 @@ const getProducts = async (): Promise<cartItemsType[]> => {
 };
 
 function App() {
-  const { data, isLoading } = useQuery<cartItemsType[]>(
-    "products",
-    getProducts
-  );
+  const [openCart, setOpenCart] = useState(false);
+  const [cartItems, setCartItems] = useState([] as cartItemsType[]);
+  const { data } = useQuery<cartItemsType[]>("products", getProducts);
 
   console.log(data);
 
   //functions handling the app's behavior
 
   //get total items in the cart
-  const getTotalItems = () => null;
+  const getTotalItems = (items: cartItemsType[]) => null;
 
   //adding items to the cart
   const addItems = (addedItem: cartItemsType) => null;
@@ -46,13 +46,25 @@ function App() {
   const removeItems = () => null;
 
   return (
-    
     <Wrapper>
+      <Drawer anchor="right" open={openCart} onClose={() => setOpenCart(false)}>
+        here are the cart items
+      </Drawer>
+      <button
+        className="cart-icon fixed z-[100] right-5 top-5 text-2xl"
+        onClick={() => setOpenCart(true)}
+      >
+        <Badge badgeContent={getTotalItems(cartItems)} color="error">
+          <FaShoppingCart />
+        </Badge>
+      </button>
       <Grid container spacing={3}>
         {data?.map((item) => {
-          return <Grid item key={item.id} xs={12} sm={4}>
-            <Item item={item} addItems={addItems} />
-          </Grid>
+          return (
+            <Grid item key={item.id} xs={12} sm={4}>
+              <Item item={item} addItems={addItems} />
+            </Grid>
+          );
         })}
       </Grid>
     </Wrapper>
